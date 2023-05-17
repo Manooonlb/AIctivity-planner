a7dfc7bf10e78f2aeacda0f5f19bffc7cdc15837
Manooonlb <[manonlambert.lb@gmail.com](mailto:manonlambert.lb@gmail.com)>
2023-05-17 09:57:25 +0200

    new entities for chat feature + implement Mercure

---
+++ api/migrations/Version20230516124137.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230516124137 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE message_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE message (id INT NOT NULL, author_id INT DEFAULT NULL, content VARCHAR(3000) DEFAULT NULL, date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_B6BD307FF675F31B ON message (author_id)');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307FF675F31B FOREIGN KEY (author_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE message_id_seq CASCADE');
+        $this->addSql('ALTER TABLE message DROP CONSTRAINT FK_B6BD307FF675F31B');
+        $this->addSql('DROP TABLE message');
+    }
+}
