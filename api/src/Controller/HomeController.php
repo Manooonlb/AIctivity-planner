47c122c056f899a3743fbd7ddcb35e03bc51afb3
CoHillDev <[colinemv.dev@gmail.com](mailto:colinemv.dev@gmail.com)>
2023-05-23 10:07:41 +0200

    Navbar / Logo / Landing

--- api/src/Controller/HomeController.php
+++ api/src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use App\Entity\User;
 
 class HomeController extends AbstractController
 {
