0cec00fa7fd9f6d8771245d010b13f07896b2de6
Manooonlb <[manonlambert.lb@gmail.com](mailto:manonlambert.lb@gmail.com)>
2023-06-07 10:48:45 +0200

    Nav paper plane not here if conversation = 0

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -431,7 +431,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-        
     public function getUnreadMessageCount(): int
     {
         $unreadMessageCount = 0;
